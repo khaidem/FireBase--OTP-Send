119b854b57065c40cf63c54d86f5c0a2db950995
khaidem <khaidemdenish@gmail.com>
2021-12-30 17:00:27 +0530

    otp

---
+++ lib/phone_auth/pages/login.page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  final _phoneController = TextEditingController();
+  final _passController = TextEditingController();
+
+  LoginScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      padding: const EdgeInsets.all(32),
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Login",
+              style: TextStyle(
+                  color: Colors.lightBlue,
+                  fontSize: 36,
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  enabledBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey)),
+                  focusedBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey)),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  hintText: "Phone Number"),
+              controller: _phoneController,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  enabledBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey)),
+                  focusedBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      borderSide: BorderSide(color: Colors.grey)),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  hintText: "Password"),
+              controller: _passController,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                child: const Text("Login"),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
---
+++ lib/phone_auth/pages/pages.dart
@@ -0,0 +1,2 @@
+export './home.page.dart';
+export './login.page.dart';
